9b19f035c303ee4352e47b6364e3c4cc48de88ad
Anubhav More <anubhav@more.com>
2024-03-09 04:37:33 +0530

    store

---
+++ rfs website/levyug/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import hero from "./components/hero"
+
+function App() {
+  return (
+    <hero/>
+  );
+}
+
+export default App;
---
+++ rfs website/levyug/src/components/hero.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from "./hero.module.css"
+
+function hero() {
+  return (
+    <div>
+    <div >
+        <div >
+            <div>l</div>
+            <div>e</div>
+            <div>v</div>
+        </div>
+        <div>
+            <div>y</div>
+            <div>u</div>
+            <div>g</div>
+        </div>
+    </div>
+    <div class="img-holder">
+            <img src="./souvik-laha-SBHrS9zs9Lo-unsplash.jpg" style="width: 100%;height:1000px;" alt="" srcset="">
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default hero
